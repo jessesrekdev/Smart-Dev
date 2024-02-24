397af91db5dd968f46ceca99e70959ea33a0553e
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-02-24 18:06:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.getElementById("downloadButton").addEventListener("click", function() {
-        window.location.href = "https://download1979.mediafire.com/7269giy3vt0g1vjHD-wxzT65CRQiSlYqDQ4ftssUgv2RmlP8SQaitcmKimR8BAWG6IJO_48pIy6N3s3g1nqx6h-tyH-48GfTDtOFFHJy852B1yy6MSCVZ3mSQuYZkVoNqZzmeWVO-oPowcO8-TMmQK8C2ToCZQvBHuHv5b4OUs7tww/4kjd2mm69bxmt33/Smart+Dev_2.0_Update.apk";
-    });
+
 const buttons = document.querySelectorAll(".button");
 
     buttons.forEach(button => {
