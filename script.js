21f92164cbb09c13d10a4c593014b51c84add04f
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-02-24 17:41:07 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".button");
+
+    buttons.forEach(button => {
+      button.addEventListener("click", (e) => {
+        e.preventDefault();
+        button.classList.add("animate");
+        setTimeout(() => {
+          button.classList.remove("animate");
+        }, 600);
+      });
+    });
