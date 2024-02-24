24bd3939f9852cddf4a837610e09b90380cf3b44
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-02-24 18:26:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ buttons.forEach(button => {
     button.classList.add("animate");
     setTimeout(() => {
       button.classList.remove("animate");
-      showToast("Downloading Pleasewait 2 secondsas we prepair apk file 💝");
+      showToast("Downloading Please wait 2 seconds as we prepare apk file for you 💝");
       setTimeout(() => {
         window.location.href = "https://download1979.mediafire.com/p03d64uqqmlghaAJHPGELdjyPe1APWXwPJKy_IIOTj-HHlnmY1CDfK8fHyhwE-7nl0SbFQuTSEBjDkXuCBPiBSwr6_PVzO2JW1D4RBqEIJd19zC-4-4EIqbyWVfwUA4IFI7ugUJs8lxiiZLs7Gk4PEEhHrDocPQxPfv29Xkq9bhROuo/4kjd2mm69bxmt33/Smart+Dev_2.0_Update.apk/file";
       }, 1000);
@@ -16,5 +16,5 @@ buttons.forEach(button => {
 
 function showToast(message) {
   // Replace this with your toast implementation, like using a library or custom code
-  alert(message);
+  alert(preparing download);
 }
