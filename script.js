4924ade88d35276ae6322c8a6df9cc7b8b9de207
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-02-24 18:13:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-
 const buttons = document.querySelectorAll(".button");
 
-    buttons.forEach(button => {
-      button.addEventListener("click", (e) => {
-        e.preventDefault();
-        button.classList.add("animate");
-        setTimeout(() => {
-          button.classList.remove("animate");
-        }, 600);
-      });
-    });
+buttons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    button.classList.add("animate");
+    setTimeout(() => {
+      button.classList.remove("animate");
+      showToast("Downloading...");
+      setTimeout(() => {
+        window.location.href = "https://www.mediafire.com/file/4kjd2mm69bxmt33/Smart+Dev_2.0_Update.apk/file";
+      }, 2000);
+    }, 600);
+  });
+});
+
+function showToast(message) {
+  // Replace this with your toast implementation, like using a library or custom code
+  alert(message);
+}
